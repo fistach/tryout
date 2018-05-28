85548ab155ea7fa7c6de3bb35bffe46a55913a04
fi stach <fistach@gmail.com>
2018-05-28 10:20:48 +0200

    Add example

---
+++ tryout/src/main/groovy/Container.groovy
@@ -0,0 +1,10 @@
+class Container {
+
+    Long id
+    BigDecimal amount
+
+    Container(Long id, BigDecimal amount) {
+        this.id = id
+        this.amount = amount
+    }
+}
---
+++ tryout/src/main/groovy/Main.groovy
@@ -0,0 +1,6 @@
+class Main {
+
+    static void main(String[] args) {
+        def x = [id: 100 as Long, amount: 12304.0G] as Container
+    }
+}
